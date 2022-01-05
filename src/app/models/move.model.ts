a35be18d0a2b5f20026fb355e437a7812ca80ac0
Adi <blue.vixen@gmail.com>
2022-01-05 15:50:44 +0200

    Auto-generated commit [ci skip]

--- src/app/models/move.model.ts
+++
@@ -1,9 +0,0 @@
-export class MoveModel {
-
-    constructor(
-        public toId: string = '',
-        public to: string = '',
-        public at: number = 0,
-        public amount: number = 0) {
-    }
-}
--- src/app/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-import { MoveModel } from './move.model';
-
-export class User {
-
-    constructor(public name: string = '', public coins: number = 100, public moves: MoveModel[] = []) {
-    }
-}
